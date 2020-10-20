f417ad84509d78ba3955ef309c80e145f88aa8cb
Your Name <you@example.com>
2020-10-20 16:09:46 +0800

    '提交'

--- src/components/CareerList/index.vue
+++ src/components/CareerList/index.vue
@@ -1,9 +1,8 @@
 <template>
-  <ul class="more-select">
-    <li v-for="(item, i) in 3"
+  <ul class="more-select flex-hb">
+    <li class="item" v-for="(item, i) in 3"
         :key="i">
-      <el-image class="logo"
-                src=""></el-image>
+      <div class="logo"></div>
       <h1>Google</h1>
       <p>由拉里·佩奇和谢尔盖·布林共同创建，被公认为全球最大的搜索引擎公司。谷歌是一家位于美国的跨国科技企业，业务包括互联网搜索…</p>
       <el-button class="learn-more"
@@ -27,32 +26,28 @@ export default {
 </script>
 
 <style lang="scss" scoped>
-.more-select {
-  display: flex;
-  justify-content: space-between;
-  flex-wrap: wrap;
+.item {
+ flex: 1;
+ margin-right: 20px;
 }
-.more-select > li {
-  width: 280px;
+.item:last-child {
+  margin-right: 0;
 }
-.more-select > li > .logo {
-  width: 280px;
+.item > .logo {
   height: 150px;
   background: #FFFFFF;
   box-shadow: 0px 0px 4px 0px rgba(0, 0, 0, 0.1);
   border-radius: 8px;
 }
-.more-select > li > h1 {
+.item > h1 {
   font-size: 14px;
   color: #7C8EA5;
   font-weight: 600;
-  padding: 0 20px;
   margin: 20px 0 10px 0;
 }
-.more-select > li > p {
+.item > p {
   font-weight: 400;
   color: #7C8EA5;
-  padding: 0 20px;
   margin-bottom: 20px;
   height: 57px;
   display: -webkit-box;    
