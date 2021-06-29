dc94f3f41038dc493bf9240c19e1eac0ad368002
Your Name <you@example.com>
2021-06-29 15:07:12 +0800

    fix

--- src/components/Scheduler/index.vue
+++ src/components/Scheduler/index.vue
@@ -65,7 +65,7 @@ export default {
       eventSettings: {
         dataSource: this.events,
         fields: {
-          // subject: { name: 'Subject', default: '咨询可预约时间' },
+          subject: { name: 'Subject', default: 'Slot' },
           startTime: { name: 'StartTime', validation: { required: [true, '请选择开始时间'] } },
         },
       },
