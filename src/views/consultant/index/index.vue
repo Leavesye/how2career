db25f558cb89d59e7158176f47a6da341f462b64
Your Name <you@example.com>
2021-10-21 14:06:45 +0800

    fix

--- src/views/consultant/index/index.vue
+++ src/views/consultant/index/index.vue
@@ -1,61 +1,104 @@
 <template>
-  <section style="padding: 30px"
-           v-if="isLoaded">
+  <section
+    style="padding: 30px"
+    v-if="isLoaded"
+  >
     <!-- 未认证简历或者没有填写服务时间 -->
     <div v-if="!isFinishReview || !isSettingTime">
-      <el-image class="banner"
-                fit="contain"
-                :src="bannerImg"></el-image>
+      <el-image
+        class="banner"
+        fit="contain"
+        :src="bannerImg"
+      ></el-image>
       <p class="reg-success">你已经成功注册咨询师</p>
       <p class="condition">只需再完成以下2个步骤即可开始接受咨询</p>
       <section class="flex-hb">
         <!-- 步骤1 -->
-        <div class="step"
-             :class="{active: !isFinishReview }">
-          <el-image class="step-num"
-                    :src="step1"></el-image>
+        <div
+          class="step"
+          :class="{active: !isFinishReview }"
+        >
+          <el-image
+            class="step-num"
+            :src="step1"
+          ></el-image>
           <!-- 审核中 -->
-          <el-image v-if="isFillResume && !isFinishReview && !isGoSign"
-                    class="status-img"
-                    :src="certApply"></el-image>
-          <div class="step-review"
-               v-if="isFillResume && !isFinishReview && !isGoSign">
+          <el-image
+            v-if="isFillResume && !isFinishReview && !isGoSign && !isWaitingCorrection"
+            class="status-img"
+            :src="certApply"
+          ></el-image>
+          <div
+            class="step-review"
+            v-if="isFillResume && !isFinishReview && !isGoSign && !isWaitingCorrection"
+          >
             <p>审核中</p>
             <p>你也可以设置服务时间</p>
           </div>
           <!-- 劳务合同签署 -->
-          <div class="step-btn" style="width: 230px" v-if="isGoSign" 
-               @click="handleOpenWindow">点击进行劳务合同签署</div>
+          <div
+            class="step-btn"
+            style="width: 230px"
+            v-if="isGoSign"
+            @click="handleOpenWindow"
+          >点击进行劳务合同签署</div>
+          <!-- 资料待修正 -->
+          <div
+            class="step-fill"
+            v-if="isWaitingCorrection"
+          >
+            <p>您的资料有缺失或者错误未通过审核</p>
+          </div>
+          <div
+            class="step-btn"
+            style="width: 230px"
+            v-if="isWaitingCorrection"
+            @click="linkTo('/consultant/resume')"
+          >资料修正</div>
           <!-- 审核完成 -->
-          <el-image v-if="isFinishReview"
-                    class="status-img"
-                    :src="certFinish"></el-image>
-          <div class="step-review"
-               v-if="isFinishReview">
+          <el-image
+            v-if="isFinishReview"
+            class="status-img"
+            :src="certFinish"
+          ></el-image>
+          <div
+            class="step-review"
+            v-if="isFinishReview"
+          >
             <p>审核完成</p>
             <p>请设置服务时间</p>
           </div>
           <!-- 填写简历 -->
-          <div class="step-fill"
-               v-if="!isFillResume">
+          <div
+            class="step-fill"
+            v-if="!isFillResume"
+          >
             <p>成为咨询师需要认证简历信息</p>
             <p>请先完成简历填写</p>
           </div>
-          <div v-if="!isFillResume"
-               class="step-btn"
-               @click="linkTo('/consultant/resume')">完善个人信息</div>
+          <div
+            v-if="!isFillResume"
+            class="step-btn"
+            @click="linkTo('/consultant/resume')"
+          >完善个人信息</div>
         </div>
         <!-- 步骤2 -->
-        <div class="step"
-             :class="{active: isFinishReview}">
-          <el-image class="step-num"
-                    :src="step2"></el-image>
+        <div
+          class="step"
+          :class="{active: isFinishReview}"
+        >
+          <el-image
+            class="step-num"
+            :src="step2"
+          ></el-image>
           <div class="step-setting">
             <p>你也可以设置您的服务时间</p>
             <p>以便通过审核后立即开始接受订单</p>
           </div>
-          <div class="step-btn"
-               @click="linkTo('/consultant/setting/1')">服务时间设置</div>
+          <div
+            class="step-btn"
+            @click="linkTo('/consultant/setting/1')"
+          >服务时间设置</div>
         </div>
       </section>
     </div>
@@ -63,11 +106,13 @@
     <div v-else>
       <!-- 状态面板 -->
       <div class="pannel flex-hb">
-        <div class="p-item"
-             :class="{ active: isActive == i }"
-             v-for="(o, i) in pannels"
-             :key="i"
-             @click="handleClickPannel(o, i)">
+        <div
+          class="p-item"
+          :class="{ active: isActive == i }"
+          v-for="(o, i) in pannels"
+          :key="i"
+          @click="handleClickPannel(o, i)"
+        >
           <p>{{o.name}}</p>
           <div class="order-count">{{o.count|num}}</div>
         </div>
@@ -75,9 +120,11 @@
       <!-- 表格 -->
       <section class="order-card">
         <p class="title">即将开始的咨询</p>
-        <ul class="list-item flex-hbc"
-            v-for="(o, i) in serviceOrders"
-            :key="i">
+        <ul
+          class="list-item flex-hbc"
+          v-for="(o, i) in serviceOrders"
+          :key="i"
+        >
           <li class="flex-hbc">
             <small-avatar :imgUrl="o.avatar"></small-avatar>
             <div>{{o.name}}</div>
@@ -85,23 +132,34 @@
           <li>开始时间: {{o.startTime}}</li>
           <li>{{o.serviceCountDown}}</li>
           <li class="flex-hb">
-            <el-button type="primary"
-                       v-if="o.status=='5'"
-                       @click="linkTo('/consultant/room/'+o.orderId)">进入房间</el-button>
-            <el-button plain
-                       @click="handleOpenDetail(o)">订单详情</el-button>
+            <el-button
+              type="primary"
+              v-if="o.status=='5'"
+              @click="linkTo('/consultant/room/'+o.orderId)"
+            >进入房间</el-button>
+            <el-button
+              plain
+              @click="handleOpenDetail(o)"
+            >订单详情</el-button>
           </li>
         </ul>
-        <div class="more"
-             v-if="serviceOrders && serviceOrders.length"
-             @click="linkTo('/consultant/order/4,5')">更多订单<i class="el-icon-arrow-right"></i></div>
-        <p v-else class="no-data">暂无数据</p>
+        <div
+          class="more"
+          v-if="serviceOrders && serviceOrders.length"
+          @click="linkTo('/consultant/order/4,5')"
+        >更多订单<i class="el-icon-arrow-right"></i></div>
+        <p
+          v-else
+          class="no-data"
+        >暂无数据</p>
       </section>
       <section class="order-card">
         <p class="title">即将超时确认的订单</p>
-        <ul class="list-item flex-hbc"
-            v-for="(o, i) in confirmOrders"
-            :key="i">
+        <ul
+          class="list-item flex-hbc"
+          v-for="(o, i) in confirmOrders"
+          :key="i"
+        >
           <li class="flex-hbc">
             <small-avatar :imgUrl="o.avatar"></small-avatar>
             <div>{{o.name}}</div>
@@ -109,165 +167,188 @@
           <li>创建时间: {{o.cTime}}</li>
           <li>{{o.confirmCountDown}}</li>
           <li class="flex-hb">
-            <el-button plain
-                       @click="handleOpenDetail(o)">订单详情</el-button>
+            <el-button
+              plain
+              @click="handleOpenDetail(o)"
+            >订单详情</el-button>
           </li>
         </ul>
-        <div class="more"
-             v-if="serviceOrders && confirmOrders.length"
-             @click="linkTo('/consultant/order/2')">更多订单<i class="el-icon-arrow-right"></i></div>
-        <p v-else class="no-data">暂无数据</p>
+        <div
+          class="more"
+          v-if="serviceOrders && confirmOrders.length"
+          @click="linkTo('/consultant/order/2')"
+        >更多订单<i class="el-icon-arrow-right"></i></div>
+        <p
+          v-else
+          class="no-data"
+        >暂无数据</p>
       </section>
     </div>
-    <detail-modal :isShow="isShow" :order="order" @close="handleClose"></detail-modal>
-    <terms :isShow="isShowTerms" @close="handleCloseTerms" :url="url" :pageCount="pageCount" />
+    <detail-modal
+      :isShow="isShow"
+      :order="order"
+      @close="handleClose"
+    ></detail-modal>
+    <terms
+      :isShow="isShowTerms"
+      @close="handleCloseTerms"
+      :url="url"
+      :pageCount="pageCount"
+    />
   </section>
 </template>
 
 <script>
-import { getOrders, getOrdersCount } from '@/api/order'
-import { getUserInfo, getDicts } from '@/api/user'
-import tool from '@/utils/tool'
-import SmallAvatar from '@/components/SmallAvatar'
-import DetailModal from './modal/detail'
+import { getOrders, getOrdersCount } from "@/api/order";
+import { getUserInfo, getDicts } from "@/api/user";
+import tool from "@/utils/tool";
+import SmallAvatar from "@/components/SmallAvatar";
+import DetailModal from "./modal/detail";
 import Terms from "@/components/Terms";
 
 export default {
-  components: { 
+  components: {
     SmallAvatar,
     DetailModal,
-    Terms
+    Terms,
   },
-  data () {
+  data() {
     return {
       isShow: false,
       isShowTerms: false,
       pageCount: 1,
-      url: '',
+      url: "",
       order: {},
       isLoaded: false,
       isFillResume: false,
       isFinishReview: false,
       isSettingTime: false,
       isGoSign: false,
+      isWaitingCorrection: false,
       isActive: false,
       curStep: 1,
       pannels: [
-        { name: '待确认订单', count: 0, path: '/consultant/order/2' },
-        { name: '待服务订单', count: 0, path: '/consultant/order/4,5' },
-        { name: '已完成订单', count: 0, path: '/consultant/order/0,7,8' },
-        { name: '待结算订单', count: 0, path: '/consultant/cost/2' }
+        { name: "待确认订单", count: 0, path: "/consultant/order/2" },
+        { name: "待服务订单", count: 0, path: "/consultant/order/4,5" },
+        { name: "已完成订单", count: 0, path: "/consultant/order/0,7,8" },
+        { name: "待结算订单", count: 0, path: "/consultant/cost/2" },
       ],
       serviceOrders: [],
-      confirmOrders: []
-    }
+      confirmOrders: [],
+    };
   },
   computed: {
     bannerImg: function () {
-      return require('../../../assets/invite.png')
+      return require("../../../assets/invite.png");
     },
     step1: function () {
-      return require('../../../assets/step1.svg')
+      return require("../../../assets/step1.svg");
     },
     step2: function () {
-      return require('../../../assets/step2.svg')
+      return require("../../../assets/step2.svg");
     },
     certApply: function () {
-      return require('../../../assets/cert-apply.png')
+      return require("../../../assets/cert-apply.png");
     },
     certFinish: function () {
-      return require('../../../assets/cert-finish.png')
-    }
+      return require("../../../assets/cert-finish.png");
+    },
   },
-  async created () {
-    const l = this.loading()
-    const res = await getUserInfo().catch(e => l.close())
+  async created() {
+    const l = this.loading();
+    const res = await getUserInfo().catch((e) => l.close());
     if (res.result) {
-      this.isLoaded = true
+      this.isLoaded = true;
       // 无resume  就是未填写
       // backgroundVerifyStatus= 0  未审核
       // backgroundVerifyStatus= 1  待审核
       // backgroundVerifyStatus= 2  资料待修正
       // backgroundVerifyStatus= 3  劳务合同签署
       // backgroundVerifyStatus= 4  已审核
-      const o = res.msg
-      // o.backgroundVerifyStatus = 1
+      const o = res.msg;
+      // o.backgroundVerifyStatus = 4;
       // 是否填写过简历
-      this.isFillResume = !!(o.publicInfo && o.publicInfo.resume)
+      this.isFillResume = !!(o.publicInfo && o.publicInfo.resume);
       // 是否审核完成
-      this.isFinishReview = o.backgroundVerifyStatus == 4
+      this.isFinishReview = o.backgroundVerifyStatus == 4;
       // 去劳务合同签署
-      this.isGoSign = o.backgroundVerifyStatus == 3
+      this.isGoSign = o.backgroundVerifyStatus == 3;
+      // 资料待修正
+      this.isWaitingCorrection = o.backgroundVerifyStatus == 2;
       // 是否设置过服务时间
-      this.isSettingTime = !!(o.publicInfo && o.publicInfo.availableTime)
+      this.isSettingTime = !!(o.publicInfo && o.publicInfo.availableTime);
       // 劳务合同签署地址
-      this.signUrl = o.eSignUrl
-      const p = { from: "0", to: "2601444690", page: "1", limit: "3" }
+      this.signUrl = o.eSignUrl;
+      const p = { from: "0", to: "2601444690", page: "1", limit: "3" };
       const ret = await Promise.all([
         getOrders({ ...p, condition: "status==4:status==5" }),
         getOrders({ ...p, condition: "status==2" }),
-        getOrdersCount({ condition: "status==2:status==4:status==7:status==10" })
-      ]).catch(e => l.close())
+        getOrdersCount({
+          condition: "status==2:status==4:status==7:status==10",
+        }),
+      ]).catch((e) => l.close());
       // 待服务订单
       if (ret[0].result) {
-        this.serviceOrders = tool.formatConsultantOrder(ret[0].msg.list)
+        this.serviceOrders = tool.formatConsultantOrder(ret[0].msg.list);
         // 缓存status==5服务中订单号
-        const servicing = this.serviceOrders[0]
-        if (servicing && servicing.status == '5') {
-          this.$store.dispatch('room/setRoom', servicing.orderId)
+        const servicing = this.serviceOrders[0];
+        if (servicing && servicing.status == "5") {
+          this.$store.dispatch("room/setRoom", servicing.orderId);
         }
       }
       // 待确认订单
       if (ret[1].result) {
-        this.confirmOrders = tool.formatConsultantOrder(ret[1].msg.list)
+        this.confirmOrders = tool.formatConsultantOrder(ret[1].msg.list);
       }
       // 订单数量
       if (ret[2].result) {
-        const info = ret[2].msg
-        this.pannels[0].count = info['2']
-        this.pannels[1].count = info['4']
-        this.pannels[2].count = info['7']
-        this.pannels[3].count = info['10']
+        const info = ret[2].msg;
+        this.pannels[0].count = info["2"];
+        this.pannels[1].count = info["4"];
+        this.pannels[2].count = info["7"];
+        this.pannels[3].count = info["10"];
       }
     }
-    l.close()
+    l.close();
   },
   methods: {
     handleCloseTerms(isAgree, isClose) {
-      if (isAgree && this.url == '/html/err-manage-consultant.html' ) {
-        window.open(this.signUrl, '_blank')
+      if (isAgree && this.url == "/html/err-manage-consultant.html") {
+        window.open(this.signUrl, "_blank");
       }
-      if ((this.url == '/html/err-manage-consultant.html' || !isAgree) || isClose) {
-        this.isShowTerms = false
-      } else if (this.url == '/html/manage-consultant.html' && isAgree) {
-        this.url = '/html/err-manage-consultant.html'
-        this.pageCount = 3
+      if (
+        this.url == "/html/err-manage-consultant.html" ||
+        !isAgree ||
+        isClose
+      ) {
+        this.isShowTerms = false;
+      } else if (this.url == "/html/manage-consultant.html" && isAgree) {
+        this.url = "/html/err-manage-consultant.html";
+        this.pageCount = 3;
       }
     },
     handleOpenWindow() {
-      this.isShowTerms = true
-      this.url = "/html/manage-consultant.html"
+      this.isShowTerms = true;
+      this.url = "/html/manage-consultant.html";
     },
-    async handleOpenDetail(order){
-      const res = await getDicts()
-      this.order = order
-      this.isShow = true
+    async handleOpenDetail(order) {
+      const res = await getDicts();
+      this.order = order;
+      this.isShow = true;
     },
     handleClose() {
-      this.isShow = false
+      this.isShow = false;
     },
-    handleClickPannel (item, i) {
-      this.isActive = i
-      this.linkTo(item.path)
+    handleClickPannel(item, i) {
+      this.isActive = i;
+      this.linkTo(item.path);
     },
-    linkTo (path) {
-      this.$router.push(path)
+    linkTo(path) {
+      this.$router.push(path);
     },
   },
-  mounted () {
-
-  }
-}
+  mounted() {},
+};
 </script>
 
 <style lang="scss" scoped>
@@ -291,7 +372,7 @@ $color: #15479e;
   align-items: center;
 }
 .step:last-child {
-  margin-right: 0
+  margin-right: 0;
 }
 .step.active {
   border: 1px solid $color;
@@ -360,8 +441,8 @@ $color: #15479e;
   box-sizing: border-box;
   color: #7c8ea5;
   cursor: pointer;
-  border:1px solid transparent;
-  transition: all .3s;
+  border: 1px solid transparent;
+  transition: all 0.3s;
 }
 .p-item:last-child {
   margin-right: 0;
@@ -380,7 +461,7 @@ $color: #15479e;
   color: $color;
 }
 .order-count {
-  font-family: 'notosans-bold,notosans-regular';
+  font-family: "notosans-bold,notosans-regular";
   font-weight: 600;
 }
 .order-card {
@@ -388,15 +469,15 @@ $color: #15479e;
   margin-bottom: 20px;
   box-shadow: 0 2px 12px 0 rgba(0, 0, 0, 0.1);
   border-radius: 4px;
-  border: 1px solid #EBEEF5;
-  background-color: #FFFFFF;
+  border: 1px solid #ebeef5;
+  background-color: #ffffff;
   overflow: hidden;
   color: #303133;
   transition: 0.3s;
 }
 .list-item {
   font-size: 14px;
-  color:#7c8ea5;
+  color: #7c8ea5;
   padding: 14px 0;
   border-bottom: 1px solid #edeeef;
 }
