1d478b0df9f376c6b682e33af093d66f3119c6bf
Your Name <you@example.com>
2022-06-17 15:40:49 +0800

    test

--- src/views/demo/index.vue
+++ src/views/demo/index.vue
@@ -1,6 +1,9 @@
 <template>
   <div class="dashboard-container">
-    <scheduler />
+    <!-- <scheduler /> -->
+    <el-table border :data="[]" :fit="true" style="width: 100%">
+      <el-table-column v-for="(item,index) in columns" :label="item.label"
+                :prop="item.prop" :key="index" align="center" :width="item.width"></el-table-column></el-table>
   </div>
 </template>
 
@@ -14,6 +17,53 @@ export default {
   },
   data () {
     return {
+      columns: [
+          {
+              label: '日期日期日期日期日期日期日期',
+              prop: 'date',
+              width: '100px'
+          },
+            {
+              label: '姓名',
+              prop: 'name',
+              width: '100px'
+          },
+            {
+              label: '省份',
+              prop: 'province',
+              width: '100px'
+          },
+            {
+              label: '市区',
+              prop: 'city',
+              width: '100px'
+          },
+            {
+              label: '地址',
+              prop: 'address',
+              width: '100px'
+          },
+          {
+              label: '邮编',
+              prop: 'zip',
+          },
+          //   {
+          //     label: '这是测试字段01',
+          //     prop: 'test01'
+          // },
+          //   {
+          //     label: '这是测试字段02',
+          //     prop: 'test02'
+          // },
+          //   {
+          //     label: '这是测试字段03',
+          //     prop: 'test03'
+          // },
+          //   {
+          //     label: '这是测试字段04',
+          //     prop: 'test04'
+          // }
+      ]
     }
   },
   methods: {
